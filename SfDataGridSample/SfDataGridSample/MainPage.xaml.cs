41ecdd111effa45dc77b77d80a7274686092d3a8
SethupathyD <sethupathy.d@syncfusion.com>
2024-12-23 16:46:49 +0530

    Code updated.

---
+++ SfDataGridSample/SfDataGridSample/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace SfDataGridSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGrid_CellTapped(object sender, Syncfusion.Maui.DataGrid.DataGridCellTappedEventArgs e)
+        {
+            Point point = dataGrid.RowColumnIndexToPoint(e.RowColumnIndex);
+            double xCoordinate = point.X;
+            double yCoordinate = point.Y;
+            DisplayAlert("Co Ordinates", $"XCoOrdinate : {xCoordinate} YCoOrdinate : {yCoordinate}","Ok");
+        }
+    }
+}
